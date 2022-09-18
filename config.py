154e986453adc90ad7de20f3b257755acb262013
Andrew Glazko <glazko.ae@gmail.com>
2022-09-18 23:18:20 +0300

    Add config (#3)
    * add config template
    
    * fix line length
    
    * fix formatter
    
    * separate formatter
    
    * Format code
    
    * fix typo

---
+++ config.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import Union
+
+from pydantic_yaml import YamlModel
+
+
+class AppConfig(YamlModel):
+    # common
+    random_state: int
+
+    # data
+    dataset_path: Path
+    dataset_output_path: Path
+    training_dataset_path: Path
+
+    # training
+    epochs: int
+    batch_size: int
+    lr: float
+
+    @classmethod
+    def parse_raw(cls, filename: Union[str, Path] = "config.yaml", *args, **kwargs):
+        with open(filename, "r") as f:
+            data = f.read()
+        return super().parse_raw(data, *args, **kwargs)
