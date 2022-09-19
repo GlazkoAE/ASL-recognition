65097841b99390b0f9d9452e217802389ec60a39
Andrew Glazko <glazko.ae@gmail.com>
2022-09-19 15:58:10 +0300

    add training and data processing (#4)

---
+++ src/data_preparation.py
@@ -0,0 +1,28 @@
+from PIL import Image
+from tqdm import tqdm
+
+from config import AppConfig
+
+
+def main_actions(config: AppConfig):
+    images_path_list = [x for x in config.dataset_path.glob("**/*.jpg")]
+    dataset_path = config.dataset_output_path
+    dataset_path.mkdir(parents=True, exist_ok=True)
+
+    for image_path in tqdm(images_path_list, desc="Images preparation"):
+        class_name = image_path.parts[-2]  # Folder Name
+        stage_name = image_path.parts[-3]  # Train/Test/Val
+        class_folder = dataset_path / stage_name / class_name
+        class_folder.mkdir(parents=True, exist_ok=True)
+        Image.open(image_path).resize(size=config.imsize).save(
+            class_folder / image_path.name
+        )
+
+
+def main():
+    config = AppConfig.parse_raw()
+    main_actions(config=config)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/data_validation.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from tqdm import tqdm
+
+from config import AppConfig
+
+
+def main_actions(config: AppConfig):
+    images_path_list = [x for x in config.dataset_path.glob("**/*.jpg")]
+    for image_path in tqdm(images_path_list, desc="Images validation"):
+        Image.open(image_path)
+
+
+def main():
+    config = AppConfig.parse_raw()
+    main_actions(config=config)
+
+
+if __name__ == "__main__":
+    main()
