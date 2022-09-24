f81f70fe7d93057a934dcd5c0eb07570056e6230
Andrew <glazko.ae@gmail.com>
2022-09-24 22:00:43 +0000

    format code

--- triton/client.py
+++ triton/client.py
@@ -1,8 +1,10 @@
-import os, sys
-import numpy as np
-import json
-import tritongrpcclient
 import argparse
+import json
+import os
+import sys
+
+import numpy as np
+import tritongrpcclient
 
 
 def load_image(img_path: str):
@@ -10,41 +12,42 @@ def load_image(img_path: str):
     Loads an encoded image as an array of bytes.
 
     """
-    return np.fromfile(img_path, dtype='uint8')
+    return np.fromfile(img_path, dtype="uint8")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name",
-                        type=str,
-                        required=False,
-                        default="ensemble",
-                        help="Model name")
-    parser.add_argument("--image",
-                        type=str,
-                        required=True,
-                        help="Path to the image")
-    parser.add_argument("--url",
-                        type=str,
-                        required=False,
-                        default="localhost:8001",
-                        help="Inference server URL. Default is localhost:8001.")
-    parser.add_argument('-v',
-                        "--verbose",
-                        action="store_true",
-                        required=False,
-                        default=False,
-                        help='Enable verbose output')
+    parser.add_argument(
+        "--model_name", type=str, required=False, default="ensemble", help="Model name"
+    )
+    parser.add_argument("--image", type=str, required=True, help="Path to the image")
+    parser.add_argument(
+        "--url",
+        type=str,
+        required=False,
+        default="localhost:8001",
+        help="Inference server URL. Default is localhost:8001.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        required=False,
+        default=False,
+        help="Enable verbose output",
+    )
     parser.add_argument(
         "--label_file",
         type=str,
         default="/client/labels.txt",
-        help="Path to the file with text representation of available labels")
+        help="Path to the file with text representation of available labels",
+    )
     args = parser.parse_args()
 
     try:
         triton_client = tritongrpcclient.InferenceServerClient(
-            url=args.url, verbose=args.verbose)
+            url=args.url, verbose=args.verbose
+        )
     except Exception as e:
         print("channel creation failed: " + str(e))
         sys.exit(1)
@@ -59,14 +62,13 @@ if __name__ == "__main__":
     image_data = load_image(args.image)
     image_data = np.expand_dims(image_data, axis=0)
 
-    inputs.append(
-        tritongrpcclient.InferInput(input_name, image_data.shape, "UINT8"))
+    inputs.append(tritongrpcclient.InferInput(input_name, image_data.shape, "UINT8"))
     outputs.append(tritongrpcclient.InferRequestedOutput(output_name))
 
     inputs[0].set_data_from_numpy(image_data)
-    results = triton_client.infer(model_name=args.model_name,
-                                  inputs=inputs,
-                                  outputs=outputs)
+    results = triton_client.infer(
+        model_name=args.model_name, inputs=inputs, outputs=outputs
+    )
 
     output0_data = results.as_numpy(output_name)
     print(output0_data)
